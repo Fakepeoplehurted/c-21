80621af05f0ff6086716de136f00bcb5d3e22338
Fakepeoplehurted <89442138+Fakepeoplehurted@users.noreply.github.com>
2021-12-11 11:16:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ 
+
+var ball_option={
+  restitution:0.8
+}
+ball=Bodies.circle(200,100,34,ball_option)
+World.add(world,ball)
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  ellipse(ball.position.x,ball.position.y,34)
+  Engine.update(engine);
+}
+
+function hForce 
